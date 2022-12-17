2a6091e77147a571c2ebc27df965f54d9b8560e8
henriquepaulalima <henrique.lima@fluna.io>
2022-12-17 20:39:08 -0300

    Create Login component

---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,30 @@
+import styles from './Login.module.scss';
+
+export default function Login() {
+  return (
+    <div>
+      <div className={styles.main_text__container}>
+        <img src="/assets/img/Logo-03.png" alt="Logo Adopet" />
+        <p>Já tem conta? Faça seu login:</p>
+      </div>
+      <form className={styles.main_form}>
+        <div className={styles.form_item__container}>
+          <label>
+            Email
+            <input id="email" type="text" placeholder="Escolha seu melhor email" name="email_text" />
+          </label>
+          <small></small>
+        </div>
+        <div className={styles.form_item__container}>
+          <label>
+            Senha
+            <input id="password" type="password" placeholder="Crie uma senha" name="password_text" title="Sua senha deve conter no mínimo 8 caracteres, pelo menos uma letra maiúscula, uma letra minúscula e um número" />
+          </label>
+          <small></small>
+          <p className={styles.forgot_password}>Esqueci minha senha</p>
+        </div>
+        <button type="submit">Entrar</button>
+      </form>
+    </div>
+  );
+}
