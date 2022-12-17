880b3afde5a73d80ac0af2a8ee2c52c58fa7091b
henriquepaulalima <henrique.lima@fluna.io>
2022-12-17 19:54:53 -0300

    Add route to Login page

--- src/pages/Welcome/Welcome.tsx
+++ src/pages/Welcome/Welcome.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import styles from './Welcome.module.scss';
 
 export default function Welcome() {
@@ -7,8 +8,12 @@ export default function Welcome() {
       <h1>Boas-vindas!</h1>
       <p>Adotar pode mudar uma vida. Que tal buscar seu novo melhor amigo hoje? Vem com a gente!</p>
       <div className={styles.buttons_container}>
-        <button>Já tenho conta</button>
-        <button>Quero me cadastrar</button>
+        <Link to="/login">
+          <button>Já tenho conta</button>
+        </Link>
+        <Link to="/singin">
+          <button>Quero me cadastrar</button>
+        </Link>
       </div>
       <div className={styles.ilustration_container}>
         <img src="/assets/img/Ilustração-01.png" alt="Ilustração da página principal" />
