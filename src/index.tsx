8a49b0245199abf810ce029c37aa2018f0be8ce7
henriquepaulalima <henrique.lima@fluna.io>
2022-12-21 15:08:29 -0300

    Rename pages folter to views

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import RoutesControl from './pages/RoutesControl';
+import RoutesControl from './views/RoutesControl';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
